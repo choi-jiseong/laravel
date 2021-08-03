de000da255488896fadd40cf1bb7a8557cb4fac4
chli-jiseong <chlwltjd98@naver.com>
2021-08-03 13:59:30 +0900

    laravel

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,33 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
-  </head>
-<body>
-    <div class="container mt-5">
+<x-app-layout>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('write') }}
+      </h2>
+  </x-slot>
+    <div class="container flex justify-between sm:mt-2 max-w-5xl w-full mx-auto z-10 m-3 bg-white rounded-3xl p-3">
         <form action="{{ route('posts.store', ['in'=>$in]) }}" method="post" enctype="multipart/form-data">
           @csrf
             <div class="form-group">
-              <label for="title">Title</label>
-              <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
+              <label for="title" class="text-2xl font-bold">Title</label>
+              <input type="text" name="title" class="w-full rounded-3xl border-2 border-gray-200" id="title" value="{{ old('title') }}">
                 @error('title')
                     <div>{{ $message }}</div>
                 @enderror
               
             </div>
             <div class="form-group">
-              <label for="content">Content</label>
-              <textarea class="form-control" name="content" id="content">{{ old('content') }}</textarea>
+              <label for="content" class="text-2xl font-bold">Content</label>
+              <textarea  name="content" id="content">{{ old('content') }}</textarea>
                 @error('content')
                     <div>{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group">
-                <label for="file">File</label><br>
+                <label for="file" class="text-2xl font-bold">File</label><br>
                 <input class="form-control" type="file" id="file" name="imageFile">
                 @error('imageFile')
                     <div>{{ $message }}</div>
@@ -37,16 +33,9 @@
         </form>
     </div>
 
+    <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+
     <script>
-      ClassicEditor
-              .create( document.querySelector( '#content' ) )
-              .then( editor => {
-                      console.log( editor );
-              } )
-              .catch( error => {
-                      console.error( error );
-              } );
+        CKEDITOR.replace( 'content' );
     </script>
-    
-</body>
-</html>
+</x-app-layout>
